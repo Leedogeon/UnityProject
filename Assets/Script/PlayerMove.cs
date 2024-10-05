c3ef76d650ec1cb0a18534fa19ee8d6113f05757
Leedogeon <jszs98@naver.com>
2024-10-06 00:25:34 +0900

    점프추가
    점프추가

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -7,11 +7,13 @@ public class script : MonoBehaviour
 {
     private Rigidbody rigid;
     public float speed = 10f;
-    public float jump = 5f;
+    public float jumpPower = 7f;
     public float turnSpeed = 3f;
     public Vector3 moveVec;
     private float xAxis;
     private float zAxis;
+    private bool jDown;
+    private bool fDown;
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
@@ -22,12 +24,16 @@ public class script : MonoBehaviour
         GetInput();
         Move();
         Turn();
+        Jump();
+        Attack();
     }
     void GetInput()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
         zAxis = Input.GetAxisRaw("Vertical");
         moveVec = new Vector3(xAxis, 0, zAxis).normalized;
+        jDown = Input.GetButtonDown("Jump");
+        fDown = Input.GetButtonDown("Fire");
     }
     void Move()
     {
@@ -38,5 +44,19 @@ public class script : MonoBehaviour
         transform.LookAt(transform.position + moveVec);
         //transform.forward = Vector3.Lerp(transform.forward, moveVec, turnSpeed * Time.deltaTime);
     }
-
+    void Jump()
+    {
+        if (jDown)
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+        
+    }
+    void Attack()
+    {
+        if(fDown)
+        {
+            print("test");
+        }
+    }
 }
