8b7832d6ef145528fcc37b63fe8f3758b6073dfe
Leedogeon <jszs98@naver.com>
2024-10-09 16:49:22 +0900

    Cinema 이용시 필요 아직 안씀
    Cinema 이용시 필요 아직 안씀

---
+++ Assets/Script/ThirdPersonCam.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform Player;
+    public Transform PlayerObj;
+    public Rigidbody rigid;
+    public float rotationSpeed;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+/*        Vector3 veiwDir = Player.position - new Vector3(transform.position.x, Player.position.y, transform.position.z);
+        orientation.forward = veiwDir.normalized;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;*/
+
+        /*if(inputDir != Vector3.zero)
+        {
+            PlayerObj.forward = Vector3.Slerp(PlayerObj.forward, inputDir.normalized, Time.deltaTime*rotationSpeed);
+        }*/
+    }
+}
